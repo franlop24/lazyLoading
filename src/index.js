25e7f2ecfe7b55c33c5de79310c9166d257f6a87
Francisco Lopez <franlopbri@gmail.com>
2022-01-30 18:39:36 -0600

    Agregando Imagenes con JavaScript

--- src/index.js
+++ src/index.js
@@ -1,6 +1,23 @@
-/**
- * This file is just a silly example to show everything working in the browser.
- * When you're ready to start on your site, clear the file. Happy hacking!
- **/
+//Obtenes referencias a objetos existentes
+const buttonAdd = document.querySelector('#addImage');
+const appImages = document.querySelector('#images');
 
-console.log('Happy hacking :)')
+
+//Crear Imagenes
+function createImageNode (){
+    const containerImage = document.createElement('div');
+    containerImage.className = "p-4";
+    const image = document.createElement('img');
+    //image.src = "https://source.unsplash.com/random";
+    const numRand = Math.round(Math.random()*122);
+    //image.src = "https://source.unsplash.com/random";
+    image.src = `https://randomfox.ca/images/${numRand}.jpg`;
+    image.alt = "Imagen aleatoria";
+    image.className = "mx-auto s-320";
+    containerImage.appendChild(image);
+
+    appImages.appendChild(containerImage);
+} 
+
+//Button
+buttonAdd.addEventListener('click', createImageNode);
